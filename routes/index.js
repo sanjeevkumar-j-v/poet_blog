6fb24839f68cc1cf594e6010c3fafd21a4b2074e
sanjeevkumar-j-v <sanjeevkumar.jv@zohocorp.com>
2023-09-03 13:23:14 +0530

    Configure routes for user actions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var usersRouter = require("./users");
+
+// const passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Muthu\'s Blog'
+  });
 });
+
+router.use("/users", usersRouter);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
+const passport = require("passport");
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/profile", passport.checkAuthentication, function (req, res) {
+  return res.render("profile");
 });
+
+router.get("/sign-up", function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/users/profile');
+  }
+  return res.render("signup");
+});
+router.get("/sign-in", function (req, res) {
+  if(req.isAuthenticated()){
+    return res.redirect('/users/profile');
+  }
+  return res.render("signin");
+});
+
+router.post("/create", function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect('back');
+  }
+  
+  User.findOne({email: req.body.email}).then((user) => {
+  //     if(err){console.log('Error in finding user in signing up: ', err); return;}
+
+    if(!user) {
+      User.create(req.body).then((user) => {
+          // if(err){console.log('Error in creating user in signing up: ', err); return;}
+          console.log("User Created : ", user);
+          res.redirect('/users/sign-in');
+      })
+    }else{
+        return res.redirect('back');
+    }
+  });
+});
+
+router.get("/sign-out", function (req, res) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+router.post(
+  "/create-session",
+  // use passport as middleware to authenticate
+  passport.authenticate("local", { failureRedirect: "/users/sign-in" }),
+  function (req, res) {
+    // after login, redirect to faculty page by default
+    // if the user is student it will be automatically redirected to student page
+    return res.redirect('/users/profile');
+  }
+);
 
 module.exports = router;
