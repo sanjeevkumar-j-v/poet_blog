587cf45f6ac57125f8ea03a78cc2ce48b02d85a5
sanjeevkumar-j-v <sanjeevkumar.jv@zohocorp.com>
2023-09-03 13:25:38 +0530

    Implement user logging actions

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var expressLayouts = require("express-ejs-layouts");
+var cors = require('cors');
+const db = require("./config/mongoose");
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require("connect-mongo");
 
 var app = express();
 
+app.use(cors());
+app.use(expressLayouts);
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,8 +29,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// // mongo strore is used to store session cookie
+app.use(
+  session({
+    secret: "blahblah",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://vithaikkul:virutcham@cluster0.ngfqupj.mongodb.net/virutcham_store?retryWrites=true&w=majority"
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
