13d84486b8b143a437a0ec4ce6369ec4625d0805
sanjeevkumar-j-v <sanjeevkumar.jv@zohocorp.com>
2023-09-03 19:33:05 +0530

    View all posts

--- models/post.js
+++ models/post.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema(
   {
+    heading: {
+      type: String,
+      required: true
+    },
     content: {
       type: String,
       required: true
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 var usersRouter = require("./users");
 var postsRouter = require("./posts");
-
+const Post = require('../models/post');
 // const passport = require('passport');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+  var posts = await Post.find({}).sort({
+    createdAt: -1,
+  });
+
   res.render('index', {
+    posts,
     title: 'Muthu\'s Blog'
   });
 });
